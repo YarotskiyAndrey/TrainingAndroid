1201508d85c5224e63f9f8c8c4105ad774e09eba
YarotskiyAndrey <yrtsky@gmail.com>
2018-11-07 17:03:28 +0200

    RecyclerView start

--- kittenapp/src/main/java/com/example/kittenapp/MainActivity.java
+++ kittenapp/src/main/java/com/example/kittenapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.kittenapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView kittenListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void initRecyclerView(){
+        kittenListView = findViewById(R.id.kitten_list_view);
+
+        kittenListView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
