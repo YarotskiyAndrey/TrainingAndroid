7d7b6ae07462ac18794ae9d016fa1a0a30a947a6
YarotskiyAndrey <yrtsky@gmail.com>
2018-11-13 01:16:18 +0200

    profile_layout design. Error: Default Activity not found

--- kittenapp/src/main/java/com/example/kittenapp/activity/ProfileActivity.java
+++ kittenapp/src/main/java/com/example/kittenapp/activity/ProfileActivity.java
@@ -1,34 +1,69 @@
 package com.example.kittenapp.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kittenapp.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.concurrent.TimeUnit;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ImageView profileImage;
+    private ImageButton phoneButtonImage;
+    private TextView phoneText;
+    private TextView numberText;
+    private TextView nameText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        profileImage = findViewById(R.id.profileImageView);
+        phoneButtonImage = findViewById(R.id.phoneImageButton);
+        phoneText = findViewById(R.id.profilePhoneText);
+        numberText = findViewById(R.id.profileNumberTextView);
+        nameText = findViewById(R.id.profileNameTextView);
 
-        TextView textView = findViewById(R.id.textView);
-        String kittenName = "default";
         Bundle extras = getIntent().getExtras();
 
-        if(extras!=null){
-            kittenName = extras.getString("name");
+        if (extras != null) {
+            String imageUrl = extras.getString("imageUrl");
+            Picasso.get().load(imageUrl).into(profileImage);
+            phoneText.setText(extras.getString("phone"));
+            numberText.setText(extras.getString("number"));
+            nameText.setText(extras.getString("name"));
         }
-        textView.setText(kittenName);
+
+        phoneButtonImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+                Log.d("MyLogs","Start call");
+                phoneIntent.setData(Uri.parse("tel:0970460922"));
+                startActivity(phoneIntent);
+                Log.d("MyLogs","End call");
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
